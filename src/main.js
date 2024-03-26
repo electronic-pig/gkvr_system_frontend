588e9f33c033ff2537c36945bb5c0838208968fc
liyang <2234333815@qq.com>
2024-03-26 19:45:33 +0800

    完成学校详情页面的展示

--- src/main.js
+++ src/main.js
@@ -8,11 +8,9 @@ import zhCn from "element-plus/es/locale/lang/zh-cn";
 import "element-plus/dist/index.css";
 import "@/assets/css/app.css";
 import "normalize.css";
-import * as echarts from "echarts";
 const app = createApp(App);
 for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
   app.component(key, component);
 }
 app.use(createPinia());
 app.use(router).use(ElementPlus, { locale: zhCn }).mount("#app");
-app.config.globalProperties.$echarts = echarts;
--- src/views/SchoolDetail.vue
+++ src/views/SchoolDetail.vue
@@ -30,19 +30,19 @@
       </div>
     </div>
 
-    <div id="intro-wrap">
+    <div class="intro-wrap">
       <div class="pic-wrap">
         <img
           class="pic"
           :src="
             'https://static-data.eol.cn//upload/svideo/piliang_' +
-            schoolInfo.schoolId +
+            schoolId +
             '_thumb.jpg'
           "
           alt="school-pic"
         />
       </div>
-      <div id="intro" class="common-card">
+      <div class="intro common-card">
         <div class="rank-wrap">
           <div class="school_rank">
             <div class="school_rank_text">
@@ -75,6 +75,44 @@
         </div>
       </div>
     </div>
+
+    <div class="info">
+      <div class="table common-card">
+        <!-- 2022年四川省理科录取分数线 -->
+        <el-table height="1070px" :data="majorScore">
+          <el-table-column prop="spname" label="专业名称" />
+          <el-table-column prop="batch" label="录取批次" width="80px" />
+          <el-table-column prop="min" label="最低分" width="66px" />
+          <el-table-column prop="minSection" label="最低位次" width="80px" />
+        </el-table>
+      </div>
+      <div class="charts">
+        <div
+          id="score-chart"
+          class="common-card"
+          style="height: 300px; width: 100%"
+        ></div>
+        <div
+          id="level-chart"
+          class="common-card"
+          style="height: 300px; width: 100%"
+        ></div>
+        <div
+          id="rate-bar"
+          class="common-card"
+          style="height: 250px; width: 100%"
+        ></div>
+        <div class="gender common-card">
+          <el-icon :size="50" color="#22B6ED"> <Male /> </el-icon
+          >{{ schoolInfoDetail.menRate }}%
+          <div id="gender-bar" style="height: 150px; width: 140px"></div>
+          {{ schoolInfoDetail.femaleRate }}%
+          <el-icon :size="50" color="#FFB6C1">
+            <Female />
+          </el-icon>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -82,6 +120,7 @@
 import { ref, onMounted, reactive } from "vue";
 import { useRoute } from "vue-router";
 import { ElMessage, ElLoading } from "element-plus";
+import * as echarts from "echarts";
 import request from "../utils/request.js";
 
 const route = useRoute();
@@ -91,8 +130,13 @@ let schoolInfo = reactive({});
 let scLiScore = reactive({});
 let majorScore = ref([]);
 
-const year = ref("2022");
 const type = ref("理科");
+const year = ref("2022");
+
+let scoreChart;
+let levelChart;
+let rateBar;
+let genderBar;
 
 const getSchoolDetail = async () => {
   const loadingInstance = ElLoading.service({
@@ -108,6 +152,26 @@ const getSchoolDetail = async () => {
       Object.assign(schoolInfo, response.data.schoolInfo);
       Object.assign(scLiScore, response.data.scLiScore);
       majorScore.value = response.data.majorScore;
+      const scores = [
+        parseInt(response.data.scLiScore.score2020),
+        parseInt(response.data.scLiScore.score2021),
+        parseInt(response.data.scLiScore.score2022),
+      ];
+      initScoreChart(scores);
+      const academic = [
+        response.data.schoolInfoDetail.numAcademician,
+        response.data.schoolInfoDetail.numSubject,
+        response.data.schoolInfoDetail.numMaster,
+        response.data.schoolInfoDetail.numDoctor,
+      ];
+      initLevelChart(academic);
+      const rate = [
+        response.data.schoolInfoDetail.job,
+        response.data.schoolInfoDetail.postgraduate,
+        response.data.schoolInfoDetail.abroad,
+      ];
+      initRateBar(rate);
+      initgenderBar(response.data.schoolInfoDetail.menRate);
     } else {
       ElMessage.error(response.message);
     }
@@ -115,6 +179,224 @@ const getSchoolDetail = async () => {
     ElMessage.error(error);
   }
   loadingInstance.close();
+};
+
+const initScoreChart = (scores) => {
+  scoreChart = echarts.init(document.getElementById("score-chart"));
+  scoreChart.setOption({
+    title: {
+      text: "四川省分数线",
+      left: "center",
+    },
+    color: "#f5460c",
+    legend: {
+      data: ["理科"],
+      top: "8%",
+    },
+    grid: {
+      left: "4%",
+      right: "4%",
+      bottom: "0",
+      containLabel: true,
+    },
+    xAxis: [
+      {
+        type: "category",
+        boundaryGap: false,
+        data: ["2020", "2021", "2022"],
+      },
+    ],
+    yAxis: [
+      {
+        type: "value",
+        min: function (value) {
+          return value.min - 10;
+        },
+        max: function (value) {
+          return value.max + 10;
+        },
+      },
+    ],
+    series: [
+      {
+        name: "理科",
+        type: "line",
+        emphasis: {
+          focus: "series",
+        },
+        data: scores,
+      },
+    ],
+  });
+};
+
+const initLevelChart = (academic) => {
+  levelChart = echarts.init(document.getElementById("level-chart"));
+  levelChart.setOption({
+    title: {
+      text: "学术水平",
+      top: "4%",
+      left: "center",
+    },
+    grid: {
+      top: "14%",
+      left: "16%",
+      right: "2%",
+      bottom: "3%",
+    },
+    color: "#f5940c",
+    xAxis: {
+      type: "value",
+      axisLabel: {
+        show: false,
+      },
+    },
+    yAxis: {
+      type: "category",
+      data: ["院士数", "重点学科", "硕士点", "博士点"],
+    },
+    series: [
+      {
+        label: {
+          show: true,
+          formatter: "{c}",
+          fontSize: 16,
+          color: "white",
+        },
+        data: academic,
+        type: "bar",
+      },
+    ],
+  });
+};
+
+const initRateBar = (rate) => {
+  rateBar = echarts.init(document.getElementById("rate-bar"));
+  rateBar.setOption({
+    title: {
+      text: "指标比率",
+      left: "center",
+    },
+    grid: {
+      left: "4%",
+      right: "4%",
+      bottom: "0",
+      top: "10%",
+      containLabel: true,
+    },
+    xAxis: {
+      type: "value",
+    },
+    yAxis: {
+      type: "category",
+      axisTick: { show: false },
+      axisLine: { show: false },
+      data: ["就业率", "深造率", "出国率"],
+    },
+    series: [
+      {
+        type: "bar",
+        barWidth: 15,
+        data: rate,
+        label: {
+          show: true,
+          position: "right",
+          offset: [-40, -20],
+          formatter: "{c}%",
+        },
+        itemStyle: {
+          borderRadius: [0, 7, 7, 0],
+          color: function (params) {
+            var colorList = [
+              new echarts.graphic.LinearGradient(0, 0, 1, 0, [
+                {
+                  offset: 0,
+                  color: "#52E5E7", //柱图渐变色起点颜色
+                },
+                {
+                  offset: 1,
+                  color: "#130CB7", //柱图渐变色终点颜色
+                },
+              ]),
+              new echarts.graphic.LinearGradient(0, 0, 1, 0, [
+                {
+                  offset: 0,
+                  color: "#FFE985", //柱图渐变色起点颜色
+                },
+                {
+                  offset: 1,
+                  color: "#FA742B", //柱图渐变色终点颜色
+                },
+              ]),
+              new echarts.graphic.LinearGradient(0, 0, 1, 0, [
+                {
+                  offset: 0,
+                  color: "#81FBB8", //柱图渐变色起点颜色
+                },
+                {
+                  offset: 1,
+                  color: "#28C76F", //柱图渐变色终点颜色
+                },
+              ]),
+            ];
+            return colorList[params.dataIndex];
+          },
+        },
+      },
+    ],
+  });
+};
+
+const initgenderBar = (menRate) => {
+  genderBar = echarts.init(document.getElementById("gender-bar"));
+  genderBar.setOption({
+    title: {
+      text: "男女比例",
+      left: "center",
+      top: "20%",
+    },
+    grid: {
+      left: "2",
+      right: "2",
+      top: "0",
+      bottom: "0",
+    },
+    xAxis: {
+      type: "value", //坐标轴类型,分别有：
+      splitLine: { show: false }, //坐标轴在 grid 区域中的分隔线
+      axisLabel: { show: false }, //坐标轴刻度标签
+      axisTick: { show: false }, //坐标轴刻度
+      axisLine: { show: false }, //坐标轴轴线
+    },
+    yAxis: {
+      type: "category",
+      axisTick: { show: false },
+      axisLine: { show: false },
+    },
+    series: [
+      {
+        name: "进度条背景",
+        type: "bar",
+        barGap: "-100%",
+        barWidth: 10,
+        data: [100],
+        color: "#FFB6C1",
+        itemStyle: {
+          borderRadius: 10,
+        },
+      },
+      {
+        name: "男性比例",
+        type: "bar",
+        barWidth: 10,
+        data: [menRate],
+        color: "#22B6ED",
+        itemStyle: {
+          borderRadius: [10, 0, 0, 10],
+        },
+      },
+    ],
+  });
 };
 
 onMounted(getSchoolDetail);
@@ -180,7 +462,7 @@ p {
   background-color: #fff;
 }
 
-#intro-wrap {
+.intro-wrap {
   display: flex;
 }
 
@@ -195,7 +477,7 @@ p {
   border-radius: 30px;
 }
 
-#intro {
+.intro {
   display: flex;
   flex-direction: column;
 }
@@ -229,5 +511,29 @@ p {
   font-size: 30px;
   color: #f60;
   margin-right: 1px;
+}
+
+.info {
+  display: flex;
+  flex-direction: row;
+  width: 100%;
+}
+
+.table {
+  width: 60%;
+}
+
+.charts {
+  display: flex;
+  flex-direction: column;
+  width: 33%;
+}
+
+.gender {
+  height: 100px;
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
 }
 </style>
